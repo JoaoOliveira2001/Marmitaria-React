379bbc0a90198ca5f8dfdf4ea3ce593d8e9ed078
João <joaovictornascimento.rh@gmail.com>
2025-06-08 15:16:58 -0300

    feat:urlSheetsNoVersel

--- api/enviar-pedido.js
+++ api/enviar-pedido.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch';
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') return res.status(405).end();
-  const appsScriptUrl = 'https://script.google.com/macros/s/AKfycbyaWHyQ3drRMRSa60oUBw8KcbAE11fWkw54hjanPbPrBn2s9OyE_bjZzB8NNgaU9IS2/exec';
+  const appsScriptUrl = 'https://script.google.com/macros/s/AKfycbyVHR5d-yjht3C6tW9h0qzO5W6MgdKpkm9sChEEv-ebfOuEx9IxFS_oUxbDoeNsrvuS/exec';
   try {
     const result = await fetch(appsScriptUrl, {
       method: 'POST',
