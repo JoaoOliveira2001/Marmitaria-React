c6c2707beb38deb84fa44b62ab162de9e816fd35
João <joaovictornascimento.rh@gmail.com>
2025-06-12 22:38:06 -0300

    asd

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import ControlePedidos from "./pages/ControlePedidos";
 import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/controle-pedidos" element={<ControlePedidos />} />
       <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   );
