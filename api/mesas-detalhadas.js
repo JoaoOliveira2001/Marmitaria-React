5775748c46c84c6301c0d48d7c1fb2afdd578b8e
João Victor <JOAOVICTORNASCIMENTO.RH@GMAIL.COM>
2025-06-18 19:22:07 -0300

    Move API routes to /api and drop apiConfig

--- api/mesas-detalhadas.js
+++ api/mesas-detalhadas.js
@@ -1,4 +1,5 @@
-import { APPS_SCRIPT_BASE } from "../../src/apiConfig";
+const APPS_SCRIPT_BASE =
+  "https://script.google.com/macros/s/AKfycbxIcGhc0fURMzbTv5sRf5uNyQ7iLKcQ_D7JTYCyfwCY-QWGf8T3FeuJLe0KwnkJtVuH/exec";
 
 export default async function handler(req, res) {
   if (req.method !== "GET") {
